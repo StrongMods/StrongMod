8d9c22564eb558d19a0f83f6fd9b0099eb276f87
StrongMods <strongmod@protonmail.com>
2023-10-16 08:12:16 -0700

    Add 'pause' for better error message visibility.

--- start_game.bat
+++ start_game.bat
@@ -3,3 +3,4 @@ setlocal
 .\strongmod\cpython\python.exe .\strongmod\strongmod\start_game.py "Stronghold Crusader.exe"
 
 endlocal
+pause
--- start_game_extreme.bat
+++ start_game_extreme.bat
@@ -3,3 +3,4 @@ setlocal
 .\strongmod\cpython\python.exe .\strongmod\strongmod\start_game.py "Stronghold_Crusader_Extreme.exe"
 
 endlocal
+pause
